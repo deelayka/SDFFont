9b1470bcee33ce793a7b2b34430e9b628dcfab8b
Dmitry Kuznetsov <deelayka@gmail.com>
2014-12-23 20:43:08 +0300

    Extracted FontRasterizer

---
+++ SDFFont/FontRasterizer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+namespace SDFFont
+{
+    class FontRasterizer : IDisposable
+    {
+        private readonly RectangleF _rectf;
+        private readonly Rectangle _rect;
+        private readonly Bitmap _bitmap;
+        private readonly byte[,] _data;
+        private readonly byte[] _raw;
+        private int _size;
+
+        public FontRasterizer(int size)
+        {
+            _size = size;
+            _bitmap = new Bitmap(size, size, PixelFormat.Format24bppRgb);
+            _data = new byte[_size, _size];
+            _raw = new byte[_size * _size * 3];
+            _rectf = new RectangleF(0, 0, _size, _size);
+            _rect = new Rectangle(0, 0, _size, _size);
+        }
+
+        public byte[,] Rasterize(string symbol)
+        {
+            var sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+
+            using (var g = Graphics.FromImage(_bitmap))
+            {
+                g.FillRectangle(Brushes.White, _rect);
+                g.DrawString(symbol, new Font("Lucida Console", _size * 3 / 4), Brushes.Black, _rectf, sf);
+            }
+
+            var bits = _bitmap.LockBits(_rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            Marshal.Copy(bits.Scan0, _raw, 0, _raw.Length);
+            _bitmap.UnlockBits(bits);
+
+            int k = 0;
+            for (int y = 0; y < _size; y++)
+                for (int x = 0; x < _size; x++)
+                {
+                    _data[x, y] = _raw[k];
+                    k += 3;
+                }
+
+            return _data;
+        }
+
+        public void Dispose()
+        {
+            _bitmap.Dispose();
+        }
+    }
+}
--- SDFFont/Program.cs
+++ SDFFont/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Drawing;
 using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace SDFFont
@@ -16,92 +15,66 @@ namespace SDFFont
 
             int ss = inSize / outSize;
 
-            var data = new byte[inSize, inSize];
-            var raw = new byte[inSize * inSize * 3];
-
-            using (var outBmp = new Bitmap(outSize * 16, outSize * 16, PixelFormat.Format32bppArgb))
+            using (var font = new FontRasterizer(inSize))
             {
-                using (var gfx = Graphics.FromImage(outBmp))
+                using (var outBmp = new Bitmap(outSize * 16, outSize * 16, PixelFormat.Format32bppArgb))
+                {
+                    using (var gfx = Graphics.FromImage(outBmp))
 
-                    for (int cy = 0; cy < 16; cy++)
-                        for (int cx = 0; cx < 16; cx++)
-                        {
-                            Console.WriteLine("{0}/256 complete.", (cx + 16 * cy));
-                            string letter = Encoding.GetEncoding(1251).GetString(new byte[] { (byte)(cx + 16 * cy) });
-
-                            var rectf = new RectangleF(0, 0, inSize, inSize);
-                            var rect = new Rectangle(0, 0, inSize, inSize);
-                            var sf = new StringFormat();
-                            sf.Alignment = StringAlignment.Center;
-
-                            using (var bmp = new Bitmap(inSize, inSize, PixelFormat.Format24bppRgb))
+                        for (int cy = 0; cy < 16; cy++)
+                            for (int cx = 0; cx < 16; cx++)
                             {
-                                using (var g = Graphics.FromImage(bmp))
+                                Console.WriteLine("{0}/256 complete.", (cx + 16 * cy));
+                                string letter = Encoding.GetEncoding(1251).GetString(new byte[] { (byte)(cx + 16 * cy) });
+
+                                var data = font.Rasterize(letter);
+
+                                using (var bmp2 = new Bitmap(outSize, outSize, PixelFormat.Format32bppArgb))
                                 {
-                                    g.FillRectangle(Brushes.White, 0, 0, inSize, inSize);
-                                    g.DrawString(letter, new Font("Lucida Console", inSize * 3 / 4), Brushes.Black, rectf, sf);
-                                }
+                                    for (int y = 0; y < outSize; y++)
+                                        for (int x = 0; x < outSize; x++)
+                                        {
+                                            bool inner = data[x * ss + ss / 2, y * ss + ss / 2] < 128;
+                                            var minDistance = inner ? int.MaxValue : int.MinValue;
 
-                                //bmp.Save("D:\\f.png");
-
-                                var bits = bmp.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-                                Marshal.Copy(bits.Scan0, raw, 0, raw.Length);
-                                bmp.UnlockBits(bits);
-                            }
-
-                            int k = 0;
-                            for (int y = 0; y < inSize; y++)
-                                for (int x = 0; x < inSize; x++)
-                                {
-                                    data[x, y] = raw[k];
-                                    k += 3;
-                                }
-
-                            using (var bmp2 = new Bitmap(outSize, outSize, PixelFormat.Format32bppArgb))
-                            {
-                                for (int y = 0; y < outSize; y++)
-                                    for (int x = 0; x < outSize; x++)
-                                    {
-                                        bool inner = data[x * ss + ss / 2, y * ss + ss / 2] < 128;
-                                        var minDistance = inner ? int.MaxValue : int.MinValue;
-
-                                        for (int j = -distance; j <= distance; j++)
-                                            for (int i = -distance; i <= distance; i++)
-                                            {
-                                                var xi = x * ss + ss / 2 + i;
-                                                var yj = y * ss + ss / 2 + j;
-                                                if (xi < 0 || yj < 0 || xi >= inSize || yj >= inSize)
-                                                    continue;
-
-                                                bool isInner = data[xi, yj] < 128;
-
-                                                if (inner != isInner)
+                                            for (int j = -distance; j <= distance; j++)
+                                                for (int i = -distance; i <= distance; i++)
                                                 {
-                                                    var ddd = i * i + j * j;
-                                                    if (!inner)
-                                                        ddd = -ddd;
-                                                    if (inner && ddd < minDistance)
-                                                        minDistance = ddd;
-                                                    if (!inner && ddd > minDistance)
-                                                        minDistance = ddd;
-                                                }
-                                            }
-                                        bool minus = minDistance < 0;
-                                        double val = (Math.Sqrt(Math.Abs((double)minDistance)) / distance);
-                                        val = ((minus ? -val : val) + 1) / 2;
-                                        int v = (int)(val * 255);
-                                        if (v < 0)
-                                            v = 0;
-                                        if (v > 255)
-                                            v = 255;
-                                        bmp2.SetPixel(x, y, System.Drawing.Color.FromArgb(255, v, v, v));
-                                    }
+                                                    var xi = x * ss + ss / 2 + i;
+                                                    var yj = y * ss + ss / 2 + j;
+                                                    if (xi < 0 || yj < 0 || xi >= inSize || yj >= inSize)
+                                                        continue;
 
-                                //bmp2.Save("D:\\d.png");
-                                gfx.DrawImageUnscaled(bmp2, new Point(cx * outSize, cy * outSize));
+                                                    bool isInner = data[xi, yj] < 128;
+
+                                                    if (inner != isInner)
+                                                    {
+                                                        var ddd = i * i + j * j;
+                                                        if (!inner)
+                                                            ddd = -ddd;
+                                                        if (inner && ddd < minDistance)
+                                                            minDistance = ddd;
+                                                        if (!inner && ddd > minDistance)
+                                                            minDistance = ddd;
+                                                    }
+                                                }
+                                            bool minus = minDistance < 0;
+                                            double val = (Math.Sqrt(Math.Abs((double)minDistance)) / distance);
+                                            val = ((minus ? -val : val) + 1) / 2;
+                                            int v = (int)(val * 255);
+                                            if (v < 0)
+                                                v = 0;
+                                            if (v > 255)
+                                                v = 255;
+                                            bmp2.SetPixel(x, y, System.Drawing.Color.FromArgb(255, v, v, v));
+                                        }
+
+                                    //bmp2.Save("D:\\d.png");
+                                    gfx.DrawImageUnscaled(bmp2, new Point(cx * outSize, cy * outSize));
+                                }
                             }
-                        }
-                outBmp.Save("D:\\out.png");
+                    outBmp.Save("D:\\out.png");
+                }
             }
         }
     }
