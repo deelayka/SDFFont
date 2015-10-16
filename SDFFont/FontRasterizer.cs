78e315eaf9df6665b5f0a22cebde14b7ecde7fd4
Dmitry Kuznetsov <deelayka@gmail.com>
2015-10-16 21:47:33 +0300

    Reduced FontRasterizer memory traffic

--- SDFFont/FontRasterizer.cs
+++ SDFFont/FontRasterizer.cs
@@ -1,44 +1,57 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 
 namespace SDFFont
 {
-    class FontRasterizer
+    class FontRasterizer : IDisposable
     {
-        public Raster Rasterize(string symbol, int size)
+        private Bitmap _bitmap;
+        private int _size;
+        private byte[] _raw;
+
+        public FontRasterizer(int size)
         {
-            Raster raster = new Raster(size);
+            _size = size;
+            _bitmap = new Bitmap(_size, _size, PixelFormat.Format24bppRgb);
+            _raw = new byte[_size * _size * 3];
+        }
 
-            using (var bitmap = new Bitmap(size, size, PixelFormat.Format24bppRgb))
+        public Raster Rasterize(string symbol)
+        {
+            Raster raster = new Raster(_size);
+
+            var rectf = new RectangleF(0, 0, _size, _size);
+            var rect = new Rectangle(0, 0, _size, _size);
+
+            var sf = new StringFormat();
+            sf.Alignment = StringAlignment.Center;
+
+            using (var g = Graphics.FromImage(_bitmap))
             {
-                var raw = new byte[size * size * 3];
-                var rectf = new RectangleF(0, 0, size, size);
-                var rect = new Rectangle(0, 0, size, size);
-
-                var sf = new StringFormat();
-                sf.Alignment = StringAlignment.Center;
-
-                using (var g = Graphics.FromImage(bitmap))
-                {
-                    g.FillRectangle(Brushes.White, rect);
-                    g.DrawString(symbol, new Font("Lucida Console", size * 3 / 4), Brushes.Black, rectf, sf);
-                }
-
-                var bits = bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
-                Marshal.Copy(bits.Scan0, raw, 0, raw.Length);
-                bitmap.UnlockBits(bits);
-
-                int k = 0;
-                for (int y = 0; y < size; y++)
-                    for (int x = 0; x < size; x++)
-                    {
-                        raster.Data[x, y] = raw[k];
-                        k += 3;
-                    }
+                g.FillRectangle(Brushes.White, rect);
+                g.DrawString(symbol, new Font("Lucida Console", _size * 3 / 4), Brushes.Black, rectf, sf);
             }
 
+            var bits = _bitmap.LockBits(rect, ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            Marshal.Copy(bits.Scan0, _raw, 0, _raw.Length);
+            _bitmap.UnlockBits(bits);
+
+            int k = 0;
+            for (int y = 0; y < _size; y++)
+                for (int x = 0; x < _size; x++)
+                {
+                    raster.Data[x, y] = _raw[k];
+                    k += 3;
+                }
+
             return raster;
+        }
+
+        public void Dispose()
+        {
+            _bitmap.Dispose();
         }
     }
 }
--- SDFFont/Program.cs
+++ SDFFont/Program.cs
@@ -13,26 +13,27 @@ namespace SDFFont
 
             int ss = inSize / outSize;
 
-            var font = new FontRasterizer();
-            var sdf = new SDF();
+            using (var font = new FontRasterizer(inSize))
+            {
+                var sdf = new SDF();
 
-            var atlas = new Raster(outSize * 16);
+                var atlas = new Raster(outSize * 16);
 
-            for (int cy = 0; cy < 16; cy++)
-                for (int cx = 0; cx < 16; cx++)
-                {
-                    Console.WriteLine("{0}/256 complete.", (cx + 16 * cy));
-                    string letter = Encoding.GetEncoding(1251).GetString(new byte[] { (byte)(cx + 16 * cy) });
+                for (int cy = 0; cy < 16; cy++)
+                    for (int cx = 0; cx < 16; cx++)
+                    {
+                        Console.WriteLine("{0}/256 complete.", (cx + 16 * cy));
+                        string letter = Encoding.GetEncoding(1251).GetString(new byte[] { (byte)(cx + 16 * cy) });
 
-                    var raster = font.Rasterize(letter, inSize);
+                        var raster = font.Rasterize(letter);
 
-                    var fragment = sdf.Process(raster, inSize, outSize, distance, ss);
+                        var fragment = sdf.Process(raster, inSize, outSize, distance, ss);
 
-                    atlas.Draw(fragment, cx * outSize, cy * outSize);
+                        atlas.Draw(fragment, cx * outSize, cy * outSize);
+                    }
 
-                }
-
-            atlas.Save("D:\\out.png");
+                atlas.Save("D:\\out.png");
+            }
         }
     }
 }
